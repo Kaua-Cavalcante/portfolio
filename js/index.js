bce78dc5045804c9c2d681d878f316239a682b53
Kaua <kaua.ferreiracavalcante@gmail.com>
2023-12-07 22:33:46 -0300

    integrando a lib AOS

--- js/index.js
+++ js/index.js
@@ -30,11 +30,17 @@ const createTags = (projectTags) => {
   return elementTags;
 };
 
-const createProject = (project) => {
+const createProject = (project, index) => {
   const elementProject = document.createElement("a");
 
   elementProject.setAttribute("href", project.link);
   elementProject.setAttribute("target", "_blank");
+
+  elementProject.setAttribute("data-aos", "zoom-in-up");
+  elementProject.setAttribute("data-aos-duration", "800");
+  elementProject.setAttribute("data-aos-easing", "ease-in-out");
+  elementProject.setAttribute("data-aos-offset", "-100");
+  elementProject.setAttribute("data-aos-delay", 300 * (index + 1));
 
   elementProject.classList.add("project");
 
@@ -51,8 +57,8 @@ const createProject = (project) => {
 };
 
 const loadProjects = (projects) => {
-  projects.forEach((project) => {
-    elementProjects.appendChild(createProject(project));
+  projects.forEach((project, index) => {
+    elementProjects.appendChild(createProject(project, index));
   });
 };
 
